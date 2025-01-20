9a5084ec0340444a99e8cda1f39dc9524355e50a
jeevan-vaishnav <jeevanvaishnavv@gmail.com>
2025-01-20 06:27:32 +0000

    ts

--- index.ts
+++ index.ts
@@ -1,1 +1,15 @@
-console.log("hi")
+// Simple print hello world 
+let helloworld = "hello world"
+console.log(helloworld)
+// Defining Types: create a object with inrefred type which includes string and number 
+const user = {
+    name:"jeevan",
+    id:10
+}
+console.log(user)
+
+// We can explicitly describe this objects shape using interface declaration
+interface User{
+    name:string,
+    id:number
+}
